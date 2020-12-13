995f55253929898b97b7cec942e5253c2c4ac993
Cynthia <cynthia@cynthia.dev>
2020-12-13 22:53:48 +0100

    Automatic deploy

--- index.js
+++ index.js
@@ -58,6 +58,13 @@ function wrapInHooks(fn) {
   };
 }
 
+const extractMessages = wrapInHooks(function (fnMessagesWrapper) {
+  return fnMessagesWrapper({
+    channel: {
+      getGuildId: () => 'a'
+    }
+  }).props.children.type;
+});
 const extractUserPopOut = wrapInHooks(f => f({
   user: {
     isNonUserBot: () => void 0
@@ -170,6 +177,40 @@ const Endpoints = Object.freeze({
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+function createDeferred() {
+  let deferred = {};
+  deferred.promise = new Promise(resolve => Object.assign(deferred, {
+    resolve
+  }));
+  return deferred;
+}
+
+/*
+ * Copyright (c) 2020 Cynthia K. Rey, All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 const symbolHttp = Symbol('pronoundb.http');
 const cache = {};
 function fetchPronouns(platform, id) {
@@ -185,21 +226,38 @@ function fetchPronouns(platform, id) {
   return cache[platform][id];
 }
 async function fetchPronounsBulk(platform, ids) {
-  const fetcher = fetchPronounsBulk[symbolHttp];
-  const data = await fetcher(Endpoints.LOOKUP_BULK(platform, ids));
+  if (!cache[platform]) cache[platform] = {};
+  const toFetch = [];
+  const res = {};
+  const def = {};
 
-  for (const id in data) {
-    if (Object.prototype.hasOwnProperty.apply(data, id)) {
-      data[id] = Pronouns[data[id]];
+  for (const id of ids) {
+    if (cache[platform][id]) {
+      res[id] = await cache[platform][id];
+    } else {
+      def[id] = createDeferred();
+      cache[platform][id] = def[id].promise;
+      toFetch.push(id);
     }
   }
 
-  return data;
+  if (toFetch.length > 0) {
+    const fetcher = fetchPronouns[symbolHttp];
+    const data = await fetcher(Endpoints.LOOKUP_BULK(platform, toFetch));
+
+    for (const id of toFetch) {
+      const pronouns = data[id] ? Pronouns[data[id]] : null;
+      def[id].resolve(pronouns);
+      res[id] = pronouns;
+    }
+  }
+
+  return res;
 }
 
-fetchPronouns[symbolHttp] = fetchPronounsBulk[symbolHttp] = url => fetch(url, {
+fetchPronouns[symbolHttp] = url => fetch(url, {
   headers: {
-    'x-pronoundb-source': 'BetterDiscord (v0.0.0-unknown)'
+    'x-pronoundb-source': 'Extension'
   }
 }).then(r => r.json());
 
@@ -248,14 +306,7 @@ function exporter(exp) {
     }
 
     pluginWillUnload() {
-      var _MessageHeader$defaul;
-
       injections.forEach(i => injector.uninject(i));
-      const MessageHeader = webpack.getModule(['MessageTimestamp'], false);
-
-      if (MessageHeader != null && (_MessageHeader$defaul = MessageHeader.default) != null && _MessageHeader$defaul.MessageHeader) {
-        MessageHeader.default = MessageHeader.default.MessageHeader;
-      }
     }
 
   }
@@ -263,6 +314,11 @@ function exporter(exp) {
   module.exports = PronounDB;
 }
 async function getModules() {
+  const fnMessagesWrapper = await webpack.getModule(m => {
+    var _m$type;
+
+    return (_m$type = m.type) == null ? void 0 : _m$type.toString().includes('getOldestUnreadMessageId');
+  });
   const UserProfile = await webpack.getModuleByDisplayName('UserProfile');
   const fnUserPopOut = await webpack.getModuleByDisplayName('UserPopout');
   const FluxAppearance = await webpack.getModuleByDisplayName('FluxContainer(UserSettingsAppearance)');
@@ -270,6 +326,7 @@ async function getModules() {
   const UserProfileBody = extractUserProfileBody(UserProfile);
   return {
     React: webpack.React,
+    Messages: extractMessages(webpack.React, fnMessagesWrapper.type),
     MessageHeader,
     UserProfileBody,
     UserProfileInfo: extractUserProfileInfo(UserProfileBody),
@@ -307,43 +364,70 @@ async function getModules() {
 exporter(async function (settings) {
   const {
     React,
+    Messages,
     MessageHeader,
     AppearanceSettings,
     UserPopOut,
     UserProfileBody,
     UserProfileInfo
-  } = await getModules(); // Custom component
-  // todo: find a better injection path where it's possible to bulk-fetch IDs, to not spam the server
+  } = await getModules();
+  const PronounsWrapper = React.memo(props => {
+    const [pronouns, setPronouns] = React.useState({});
+    React.useEffect(() => {
+      const toFetch = [...new Set(props.items.filter(i => i.props.message && !i.props.message.author.bot).map(i => i.props.message.author.id))];
+      fetchPronounsBulk('discord', toFetch).then(setPronouns);
+    }, [props.items]);
+    const elements = React.useMemo(() => {
+      const res = [];
 
-  const MessageHeaderWithPronouns = (React => props => {
-    // doing this weird thing otherwise it gets thrown out of the promise by rollup
-    const [pronouns, setPronouns] = React.useState(null);
-    const showInChat = settings.get('showInChat', true);
-    React.useEffect(function () {
-      if (showInChat && !props.message.author.bot) {
-        fetchPronouns('discord', props.message.author.id).then(pronouns => setPronouns(pronouns));
+      for (const i of props.items) {
+        var _i$props$message;
+
+        const authorId = (_i$props$message = i.props.message) == null ? void 0 : _i$props$message.author.id;
+
+        if (authorId && pronouns[authorId]) {
+          const message = window._.clone(i.props.message);
+
+          message.__$pronouns = pronouns[authorId];
+          res.push(React.cloneElement(i, {
+            message
+          }));
+        } else {
+          res.push(i);
+        }
       }
-    }, [pronouns, showInChat, props.message.author.id]);
-    const res = MessageHeaderWithPronouns.MessageHeader.call(null, props);
 
-    if (pronouns && showInChat) {
+      return res;
+    }, [props.items, pronouns]);
+    return React.createElement(React.Fragment, null, ...elements);
+  });
+  inject(Messages, 'type', function (_, res) {
+    const ogFn = res.props.children.props.children[1].props.children;
+
+    res.props.children.props.children[1].props.children = function (e) {
+      const res = ogFn(e);
+      const items = res.props.children.props.children[1];
+      res.props.children.props.children[1] = React.createElement(PronounsWrapper, {
+        items
+      });
+      return res;
+    };
+
+    return res;
+  });
+  inject(MessageHeader, 'default', function ([props], res) {
+    if (props.message.__$pronouns) {
       res.props.children[1].props.children.push(React.createElement('span', {
         style: {
           color: 'var(--text-muted)',
-          fontSize: '.9rem'
+          fontSize: '.9rem',
+          marginRight: props.compact ? '.6rem' : ''
         }
-      }, ' • ', pronouns));
+      }, ' • ', props.message.__$pronouns));
     }
 
     return res;
-  })(React);
-
-  const ogDefault = MessageHeader.default;
-  MessageHeaderWithPronouns.displayName = 'MessageHeaderWithPronouns';
-  MessageHeaderWithPronouns.MessageHeader = ogDefault;
-  MessageHeader.default = MessageHeaderWithPronouns;
-  MessageHeader.default.toString = ogDefault.toString;
-  Object.assign(MessageHeader.default, ogDefault); // Settings
+  }); // Settings
 
   inject(AppearanceSettings.prototype, 'render', function (_, res) {
     const section = React.createElement(React.Fragment, null, React.cloneElement(res.props.children[3].props.children[0], {
